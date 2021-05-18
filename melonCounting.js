c03df94f008ec1885a05a1a4afa75808b71fc427
Tora Sirkka <tora@torasirkka.fi>
2021-05-17 17:39:49 -0700

    Translated python code to javascript found in melonCounting.js

---
+++ melonCounting.js
@@ -0,0 +1,29 @@
+
+// Our customers are going to buy lots of melons!
+
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+
+const countMelons = melon_list => {
+
+    const melon_counts = {};
+    for (const melon of melon_list){
+        if (melon_counts.hasOwnProperty(melon)) {
+            melon_counts[melon] += 1
+        }
+        else {
+            melon_counts[melon] = 0;
+        }
+    }
+    return melon_counts
+};
